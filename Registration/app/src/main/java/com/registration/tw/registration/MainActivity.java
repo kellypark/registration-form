920e34dab5fac2419084ccaa424ee69ef526f958
Jessie Leung <jleu1656@gmail.com>
2017-09-02 12:02:13 +1000

    Build basic functionality for form

--- Registration/app/src/main/java/com/registration/tw/registration/MainActivity.java
+++ Registration/app/src/main/java/com/registration/tw/registration/MainActivity.java
@@ -1,17 +1,63 @@
 package com.registration.tw.registration;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
-import static com.registration.tw.registration.R.*;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.registerButton)
+    Button registerButton;
+
+    @BindView(R.id.emailField)
+    EditText emailField;
+
+    @BindView(R.id.usernameField)
+    EditText usernameField;
+
+    @BindView(R.id.passwordField)
+    EditText passwordField;
+
+    @BindView(R.id.root)
+    LinearLayout rootContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_main);
-        
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setUpForm();
+    }
+
+    private void setUpForm() {
+        // this is meant to look bad
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!emailField.getText().toString().equals("") &&
+                        !usernameField.getText().toString().equals("") &&
+                        !passwordField.getText().toString().equals("")) {
+                    if (emailField.getText().toString().matches("^[a-zA-Z0-9_]*@[a-zA-Z0-9]*.com") &&
+                            passwordField.getText().toString().matches("^[a-zA-Z0-9_]{4,}")) {
+                        Intent intent = new Intent(MainActivity.this, RegistrationSuccessActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Snackbar.make(rootContainer, "Sorry cannot register", Snackbar.LENGTH_LONG).show();
+                    }
+                } else {
+                    Snackbar.make(rootContainer, "Sorry cannot register", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
 }
---
+++ Registration/app/src/main/java/com/registration/tw/registration/RegistrationSuccessActivity.java
@@ -0,0 +1,15 @@
+package com.registration.tw.registration;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class RegistrationSuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_success);
+    }
+}
