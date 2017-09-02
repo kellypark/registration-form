4fa64afc2b40095c75245cf15643632762066e8b
Jessie Leung <jleu1656@gmail.com>
2017-09-02 11:16:06 +1000

    Create base project with registration form

---
+++ Registration/app/src/main/java/com/registration/tw/registration/MainActivity.java
@@ -0,0 +1,17 @@
+package com.registration.tw.registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static com.registration.tw.registration.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+        
+    }
+
+}
